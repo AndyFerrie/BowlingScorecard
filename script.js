4b0384008b292431655b05741c2407cb7b5ed492
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2022-02-25 15:18:59 +0000

    Add hide/show buttons function

--- script.js
+++ script.js
@@ -19,13 +19,14 @@ let frame7 = 0
 let frame8 = 0
 let frame9 = 0
 let frame10 = 0
-let strike = false
-let spare = false
-
-let frameNum = `frame${frame}`;
 
 function bowl (num) {
     if (ball === 1) {
+        hideButtons(num);
+        if (num === 10) {
+            strike();
+            return;
+        }
         pinsHitFirst[frame-1].textContent = num;
         score += num;
         frameScore[frame-1].textContent = score;
@@ -35,9 +36,18 @@ function bowl (num) {
         score += num;
         frameScore[frame-1].textContent = score;
         nextFrame();
+        showButtons();
     }
     console.log(`frame is ${frame}`)
     console.log(`ball is ${ball}`)
+}
+
+function strike() {
+    pinsHitSecond[frame-1].textContent = "X";
+    score += 10;
+    frameScore[frame-1].textContent = score;
+    nextFrame();
+    showButtons();
 }
 
 function nextFrame() {
@@ -45,8 +55,20 @@ function nextFrame() {
     ball = 1;
 }
 
-for (let i =0; i < buttons.length; i++) {
+for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function () {
         bowl(i);
     })
+}
+
+function hideButtons (num) {
+    for (let i = (11 - num); i < buttons.length; i++) {
+        buttons[i].classList.add('hide');
+    }
+}
+
+function showButtons () {
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('hide');
+    }
 }
